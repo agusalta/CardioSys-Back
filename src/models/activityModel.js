4343f31ccc4d00e9daa3f2b7405e8d69713c32fc
Agus Altamirano <agusalta002@gmail.com>
2025-01-23 17:04:12 -0300

    .

--- src/models/activityModel.js
+++ src/models/activityModel.js
@@ -2,6 +2,6 @@ import { connection } from "../config/db.js";
 
 export const getRecentActivity = (callback) => {
   const query =
-    "SELECT 'Paciente' AS Tipo, ID_Paciente AS ID, DNI AS Paciente, CONCAT_WS(' ', Nombre, Apellido) AS Detalle, FechaCreacion AS Fecha FROM paciente WHERE FechaCreacion IS NOT NULL UNION SELECT 'Estudio' AS Tipo, e.ID_Estudio AS ID, p.DNI AS Paciente, t.NombreEstudio AS Estudio, e.Fecha AS Fecha FROM estudio e LEFT JOIN tipoestudio t ON e.ID_TipoEstudio = t.ID_TipoEstudio LEFT JOIN paciente p ON e.ID_Paciente = p.ID_Paciente WHERE e.Fecha IS NOT NULL ORDER BY Fecha DESC LIMIT 10;";
+    "SELECT 'Paciente' AS Tipo, ID_Paciente AS ID, DNI AS Paciente, CONCAT_WS(' ', Nombre, Apellido) AS Detalle, FechaCreacion AS Fecha, NULL AS ID_Paciente FROM paciente WHERE FechaCreacion IS NOT NULL UNION SELECT 'Estudio' AS Tipo, e.ID_Estudio AS ID, p.DNI AS Paciente, t.NombreEstudio AS Estudio, e.FechaCreacion AS Fecha, e.ID_Paciente AS ID_Paciente FROM estudio e LEFT JOIN tipoestudio t ON e.ID_TipoEstudio = t.ID_TipoEstudio LEFT JOIN paciente p ON e.ID_Paciente = p.ID_Paciente WHERE e.FechaCreacion IS NOT NULL ORDER BY Fecha DESC LIMIT 10;";
   connection.query(query, callback);
 };

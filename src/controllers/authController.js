38aac890770e2e6d48b1902ad825103be59985b6
Agus Altamirano <agusalta002@gmail.com>
2025-02-08 17:13:57 -0300

    Fix authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,36 +6,32 @@ export const login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    // Asegúrate de usar await ya que findUserByUsername es asincrónica
     const user = await findUserByUsername(username);
 
     if (!user) {
       return res.status(401).json({ message: "Usuario no encontrado" });
     }
 
-    // Usa await con bcrypt.compare, ya que es una función asincrónica
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
       return res.status(401).json({ message: "Credenciales incorrectas" });
     }
 
-    // Generar el token
     const token = jwt.sign(
       { username: user.username },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
-    // Opcional: guardar el token en una cookie (si es necesario)
     res.cookie("auth", token, {
-      domain: ".railway.app", // Configura el dominio principal que abarca tanto frontend como backend
+      domain:
+        process.env.NODE_ENV === "production" ? ".railway.app" : "localhost", // Usa el dominio adecuado según el entorno
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // Asegúrate de que esté en true en producción
-      sameSite: "None", // "None" permite cookies entre dominios diferentes
+      secure: process.env.NODE_ENV === "production", // Solo usa secure si es producción
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // "None" para producción, "Lax" para local
       maxAge: 3600000, // 1 hora
     });
 
-    // Enviar el token en la respuesta
     return res.json({ message: "Login exitoso", token });
   } catch (error) {
     console.error(error);
@@ -44,11 +40,12 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  // Limpiar la cookie de autenticación
   res.clearCookie("auth", {
+    domain:
+      process.env.NODE_ENV === "production" ? ".railway.app" : "localhost", // Usa el dominio adecuado según el entorno
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "None", // SameSite: "None" para permitir cookies entre dominios
+    secure: process.env.NODE_ENV === "production", // Solo usa secure si es producción
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // "None" para producción, "Lax" para local
   });
 
   res.json({ message: "Cierre de sesión exitoso" });
@@ -61,7 +58,6 @@ export const checkAuth = (req, res) => {
     return res.status(401).json({ isLoggedIn: false });
   }
 
-  // Verificación del token JWT
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).json({ isLoggedIn: false });
@@ -78,16 +74,13 @@ export const protectRoute = (req, res, next) => {
     return res.status(401).json({ message: "Token no proporcionado" });
   }
 
-  // Obtener el token del header Authorization
   const token = authHeader.split(" ")[1];
 
-  // Verificación del token JWT
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).json({ message: "Token no válido" });
     }
 
-    // Añadir el usuario decodificado a la solicitud
     req.user = decoded;
     next();
   });
